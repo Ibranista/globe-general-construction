b63cc46e1a235ed50ba748d5236d3dfe042a729f
Ibrahim <techofreac@gmail.com>
2024-01-14 15:39:50 +0300

    ft: styled theme.

---
+++ src/theme.ts
@@ -0,0 +1,33 @@
+
+const primary = {
+  main: "#3d5a80",
+  light: '#98c1d9',
+  dark: '#293241',
+  darker: '#355070'
+}
+
+const secondary = {
+  main: '#ffc300',
+  dark: '#ae8507',
+  light: '#FADA5E',
+  lighter: '#FFE12B',
+  darker: '#5a4505'
+}
+
+const black = {
+  main: '#000000',
+  light: '#2a2a2a',
+  lighter: '#3a3a3a',
+  ebony: '#080402',
+  leather: '#0B0705'
+}
+
+export const theme = {
+  primaryDark: "#0D0C1D",
+  primaryLight: "#EFFFFA",
+  primaryHover: "#343078",
+  mobile: "576px",
+  primary,
+  secondary,
+  black
+};
